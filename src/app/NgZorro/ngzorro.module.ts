b3b171924678f787dda1ab3fbf0c1735fe6dbb0e
Gareth-Vassallo <garethvassallo@gmail.com>
2023-06-05 19:19:22 +0200

    created

---
+++ src/app/NgZorro/ngzorro.module.ts
@@ -0,0 +1,17 @@
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule} from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb'
+import { NgModule } from '@angular/core';
+import {} from 'ng-zorro-antd/'
+
+@NgModule({
+    declarations: [],
+    imports: [
+        NzButtonModule,
+        NzInputModule,
+        NzLayoutModule,
+        NzBreadCrumbModule,
+    ]
+})
+export class NgZorroModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroModule } from './NgZorro/ngzorro.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDividerModule} from 'ng-zorro-antd/divider'
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+    NzGridModule,
+    NzImageModule,
+    NzIconModule,
+    NzDividerModule,
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
